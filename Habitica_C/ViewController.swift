82ac88d5be921d16d8195d77f3b6a748f2d46cd4
Vincenzo Raucci <vincy.r.98@gmail.com>
2019-12-13 12:35:54 +0100

    Initial Commit

---
+++ Habitica_C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habitica_C
+//
+//  Created by Vincenzo Raucci on 13/12/2019.
+//  Copyright © 2019 Vincenzo Raucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Habitica_CTests/Habitica_CTests.swift
@@ -0,0 +1,34 @@
+//
+//  Habitica_CTests.swift
+//  Habitica_CTests
+//
+//  Created by Vincenzo Raucci on 13/12/2019.
+//  Copyright © 2019 Vincenzo Raucci. All rights reserved.
+//
+
+import XCTest
+@testable import Habitica_C
+
+class Habitica_CTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
